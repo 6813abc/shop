90e6e1d64de2384fec197f2cfc577f6634713403
chenyg5 <chenyg5>
2020-03-16 16:41:58 +0800

    初始化

---
+++ src/main/java/com/ddw/shop/aop/ServiceAop.java
@@ -0,0 +1,50 @@
+package com.ddw.shop.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.LogRecord;
+
+/**
+ * @Description: 异常处理，统一返回
+ * @Author: cyg
+ * @Date: 2019/8/13
+ * @Version:
+ **/
+@Aspect
+@Component
+@Slf4j
+public class ServiceAop {
+
+    @Pointcut("execution(public * com.ddw.shop.controller..*.*(..))")
+    public void controllerLog() {
+    }
+
+    @Before("controllerLog()")
+    public void doBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            log.info("url={}", request.getRequestURL());
+        }
+        for (Object o : joinPoint.getArgs()){
+            log.info("args={}", o);
+        }
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "controllerLog()")
+    public void doAfter(Object ret) {
+        log.info("ret={}", ret);
+    }
+}
