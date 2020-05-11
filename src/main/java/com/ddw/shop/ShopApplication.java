30da042b59dae608a490448ca0c6f665f82559a1
chenyg5 <chenyg5>
2020-05-11 13:05:32 +0800

    1.swaggerbug

--- src/main/java/com/ddw/shop/ShopApplication.java
+++ src/main/java/com/ddw/shop/ShopApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(value = "com.ddw.shop.mapper")
+
 public class ShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
